504ec1734a539f203fb39e2455f383e7a22d02a2
Goran Mekić <meka@tilda.center>
2021-06-27 01:04:46 +0200

    Change landing

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,3 @@
 import { createMuiTheme } from '@material-ui/core/styles'
 
-export default createMuiTheme({
-  palette: {
-    primary: {
-      main: '#1b6dc1ff',
-    },
-  },
-})
+export default createMuiTheme({})
